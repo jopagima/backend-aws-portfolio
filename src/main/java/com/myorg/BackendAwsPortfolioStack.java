8e77a1872c5bcaccbcfa0e3c8209a1fdb68fd70d
tirantvcia <tirantvcia@gmail.com>
2026-06-25 08:32:16 +0200

    feat: define lambda infrastructure in CDK and perform initial deployment

--- src/main/java/com/myorg/BackendAwsPortfolioStack.java
+++ src/main/java/com/myorg/BackendAwsPortfolioStack.java
@@ -1,10 +1,13 @@
 package com.myorg;
 
 import software.constructs.Construct;
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.lambda.*;
+import software.amazon.awscdk.services.lambda.Runtime;
 
 public class BackendAwsPortfolioStack extends Stack {
     public BackendAwsPortfolioStack(final Construct scope, final String id) {
@@ -20,5 +23,16 @@ public class BackendAwsPortfolioStack extends Stack {
         // final Queue queue = Queue.Builder.create(this, "BackendAwsPortfolioQueue")
         //         .visibilityTimeout(Duration.seconds(300))
         //         .build();
+
+        // Define the Lambda function
+        Function statusLambda = Function.Builder.create(this, "StatusLambda")
+            .runtime(Runtime.JAVA_21)
+            .handler("com.portfolio.statuslambda.StatusHandler")
+            .code(Code.fromAsset("services/status-lambda/target/status-lambda-0.0.1-SNAPSHOT.jar"))
+            .memorySize(512)
+            .timeout(Duration.seconds(15))
+            .build();
+
+
     }
 }
