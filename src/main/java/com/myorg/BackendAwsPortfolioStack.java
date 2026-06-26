95cba7d3ad22f76ba72258a1f439bf645e82a1e3
tirantvcia <tirantvcia@gmail.com>
2026-06-26 17:28:43 +0200

    feat(api): expose status-lambda via API Gateway and verify public endpoint

--- src/main/java/com/myorg/BackendAwsPortfolioStack.java
+++ src/main/java/com/myorg/BackendAwsPortfolioStack.java
@@ -8,6 +8,8 @@ import software.amazon.awscdk.StackProps;
 // import software.amazon.awscdk.services.sqs.Queue;
 import software.amazon.awscdk.services.lambda.*;
 import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+import software.amazon.awscdk.services.apigateway.StageOptions;
 
 public class BackendAwsPortfolioStack extends Stack {
     public BackendAwsPortfolioStack(final Construct scope, final String id) {
@@ -21,18 +23,25 @@ public class BackendAwsPortfolioStack extends Stack {
 
         // example resource
         // final Queue queue = Queue.Builder.create(this, "BackendAwsPortfolioQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        // .visibilityTimeout(Duration.seconds(300))
+        // .build();
 
         // Define the Lambda function
         Function statusLambda = Function.Builder.create(this, "StatusLambda")
-            .runtime(Runtime.JAVA_21)
-            .handler("com.portfolio.statuslambda.StatusHandler")
-            .code(Code.fromAsset("services/status-lambda/target/status-lambda-0.0.1-SNAPSHOT.jar"))
-            .memorySize(512)
-            .timeout(Duration.seconds(15))
-            .build();
+                .runtime(Runtime.JAVA_21)
+                .handler("com.portfolio.statuslambda.StatusHandler")
+                .code(Code.fromAsset("services/status-lambda/target/status-lambda-0.0.1-SNAPSHOT.jar"))
+                .memorySize(512)
+                .timeout(Duration.seconds(15))
+                .build();
 
+        LambdaRestApi api = LambdaRestApi.Builder.create(this, "PortFolioApi")
+                .handler(statusLambda) // referencia a la función lambda
+                .proxy(true) // habilita la integración proxy
+                .deployOptions(StageOptions.builder()
+                        .stageName("prod")  //define el entorno de producción
+                        .build())
+                .build();
 
     }
 }
