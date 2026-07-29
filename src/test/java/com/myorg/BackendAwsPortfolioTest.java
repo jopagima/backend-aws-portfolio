c224e9f683ad8777bd2bb20c2edb02f4396a4567
tirantvcia <tirantvcia@gmail.com>
2026-07-29 17:31:00 +0200

    Test de grano fino para validar la arquitectura

--- src/test/java/com/myorg/BackendAwsPortfolioTest.java
+++ src/test/java/com/myorg/BackendAwsPortfolioTest.java
@@ -1,26 +1,40 @@
-// package com.myorg;
+package com.myorg;
 
-// import software.amazon.awscdk.App;
-// import software.amazon.awscdk.assertions.Template;
-// import java.io.IOException;
+ import software.amazon.awscdk.App;
+import software.amazon.awscdk.assertions.Match;
+import software.amazon.awscdk.assertions.Template;
 
-// import java.util.HashMap;
 
-// import org.junit.jupiter.api.Test;
+import java.io.IOException;
 
-// example test. To run these tests, uncomment this file, along with the
-// example resource in java/src/main/java/com/myorg/BackendAwsPortfolioStack.java
-// public class BackendAwsPortfolioTest {
+ 
+import java.util.Map;
 
-//     @Test
-//     public void testStack() throws IOException {
-//         App app = new App();
-//         BackendAwsPortfolioStack stack = new BackendAwsPortfolioStack(app, "test");
+import org.junit.jupiter.api.Test;
 
-//         Template template = Template.fromStack(stack);
 
-//         template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
-//           put("VisibilityTimeout", 300);
-//         }});
-//     }
-// }
+ /*example test. To run these tests, uncomment this file, along with the
+ example resource in java/src/main/java/com/myorg/BackendAwsPortfolioStack.java
+ */
+ public class BackendAwsPortfolioTest {
+
+     @Test
+     public void testStack() throws IOException {
+         App app = new App();
+         BackendAwsPortfolioStack stack = new BackendAwsPortfolioStack(app, "test");
+
+         Template template = Template.fromStack(stack);
+
+        
+        /* 
+        template.hasResourceProperties("AWS::SQS::Queue", new HashMap<String, Number>() {{
+           put("VisibilityTimeout", 300);
+         }});
+         */
+        template.hasResourceProperties("AWS::ApiGateway::RestApi", Match.anyValue());
+        template.hasResourceProperties("AWS::Lambda::Function", Map.of(
+            "Handler", "com.portfolio.statuslambda.StatusHandler",
+            "Runtime", "java21"
+        ));
+     }
+ }
